07472a0c4ceba3796948c37460b1e29d3fec71ca
Hotja <dudechka@mail.ru>
2021-04-11 22:29:32 +0300

    feat: st2021 - 5

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,16 +1,18 @@
-/**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For n = 152, the output should be 52
- *
- */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const str = String(n);
+  let arr = str.slice();
+  arr = arr.split('');
+
+  let res = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let x = arr.slice();
+    x.splice(i, 1);
+    x = x.join('');
+
+    if (Number(x) > res) { res = Number(x); }
+  }
+  return res;
 }
 
 module.exports = deleteDigit;
