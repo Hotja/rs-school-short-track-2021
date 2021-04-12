c17dea59e34bdff74edf35a37116efe94b6ec652
Hotja <dudechka@gmail.com>
2021-04-12 09:18:40 +0300

    feat: 6

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -1,11 +1,7 @@
 function isMAC48Address(n) {
   const mac = n.split('-');
-  console.log(mac);
   for (let i = 0; i < mac.length; i++) {
     const x = mac[i].split('');
-    console.log(x);
-    console.log(mac[i][0]);
-    console.log(mac[i][1]);
     if (!/[a-fA-F0-9]/.test(mac[i][0])) return false;
     if (!/[a-fA-F0-9]/.test(mac[i][1])) return false;
   }
