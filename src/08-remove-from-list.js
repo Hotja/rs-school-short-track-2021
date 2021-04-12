b5177008b0f02e1166782cb2f848da158e6a6174
Hotja <dudechka@gmail.com>
2021-04-12 19:52:08 +0300

    feat: 15

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -1,12 +1,6 @@
 /* function removeKFromList(l, k) {
-  console.log(l);
   console.log(k);
-  const arr = l.splice();
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === k) arr.splice(i, 1);
-  }
-  return arr;
+  return l;
 }
 
 module.exports = removeKFromList;
--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -1,15 +1,10 @@
-/**
- * Given an email address, return it's domain.
- *
- * @param {String} email
- * @return {String}
- *
- * @example
- * For the input 'prettyandsimple@example.com', the output should be 'example.com'
- *
- */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const arr = email.split('@');
+
+  if (arr[arr.length - 1][0] === '.') {
+    return arr[arr.length - 1].substr(1, arr[1].length - 1);
+  }
+  return arr[arr.length - 1];
 }
 
 module.exports = getEmailDomain;
