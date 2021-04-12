c833d113d40c1f947afe53e9a2ab75044ab44487
Hotja <dudechka@gmail.com>
2021-04-12 14:36:49 +0300

    feat: 7

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -1,7 +1,7 @@
 function isMAC48Address(n) {
   const mac = n.split('-');
   for (let i = 0; i < mac.length; i++) {
-    const x = mac[i].split('');
+    /* const x = mac[i].split(''); */
     if (!/[a-fA-F0-9]/.test(mac[i][0])) return false;
     if (!/[a-fA-F0-9]/.test(mac[i][1])) return false;
   }
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -1,20 +1,16 @@
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const arr = {};
+  const res = names.map((el) => {
+    if (arr[el]) {
+      arr[`${el}(${arr[el]})`] = 1;
+      return `${el}(${arr[el]++})`;
+    }
+
+    arr[el] = 1;
+    return el;
+  });
+
+  return res;
 }
 
 module.exports = renameFiles;
