d83e1bdf6463f65f61f9266ae39c7ba1c03fa9ae
Hotja <dudechka@gmail.com>
2021-04-12 17:52:39 +0300

    feat: 10

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,15 +1,22 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const res = [];
+  let count = 1;
+
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      count++;
+    }
+
+    if (arr[i] !== arr[i - 1]) {
+      if (count !== 1) {
+        res.push(count);
+        res.push(arr[i - 1]);
+      } else res.push(arr[i - 1]);
+      count = 1;
+    }
+  }
+  return res.join('');
 }
 
 module.exports = encodeLine;
